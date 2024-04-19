6f76a271ed07554e1385bdd2d5e29a10a8ac4ff7
brikenakastrati <116557835+brikenakastrati@users.noreply.github.com>
2024-04-19 14:07:58 +0200

    at this point nu ke di cka kom ndrru

--- ticket.php
+++ ticket.php
@@ -141,7 +141,7 @@ exit();
 
         <nav class="navbar navbar-expand-lg">
             <div class="container">
-                <a class="navbar-brand" href="index.html">Sunny Hill</a>
+                <a class="navbar-brand" href="homepage.html">Sunny Hill</a>
                 <a href="ticket.php" class="btn custom-btn d-lg-none ms-auto me-4">Buy Ticket</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -149,6 +149,7 @@ exit();
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav align-items-lg-center ms-auto me-lg-5">
                         <li class="nav-item">
+<<<<<<< Updated upstream
                             <a class="nav-link click-scroll" href="homepage.php#section_1">Home</a>
                         </li>
                         <li class="nav-item">
@@ -165,6 +166,24 @@ exit();
                         </li>
                         <li class="nav-item">
                             <a class="nav-link click-scroll" href="homepage.php#section_6">Contact</a>
+=======
+                            <a class="nav-link click-scroll" href="homepage.html#section_1">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="homepage.html#section_2">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="homepage.html#section_3">Artists</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="homepage.html#section_4">Schedule</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="homepage.html#section_5">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="homepage.html#section_6">Contact</a>
+>>>>>>> Stashed changes
                         </li>
                     </ul>
                     <a href="login.php" class="btn custom-btn d-lg-block d-none">Buy Ticket</a>
